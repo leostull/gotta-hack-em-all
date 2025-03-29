c2745452d8bf32182a6696a1ebb3fd7b4c0051da
Alexa Moss <alexa@Alexas-MacBook-Pro-4.local>
2025-03-29 03:33:27 -0600

    Hopefully fixed URL error.

--- MyHelloApp/app/src/main/java/API.java
+++ MyHelloApp/app/src/main/java/API.java
@@ -14,7 +14,7 @@ public class API {
     private String token;
 
     // Constructor
-    public Api(String baseUrl, String token) {
+    public void Api(String baseUrl, String token) {
         this.baseUrl = baseUrl;
         this.token = token;
     }
@@ -30,7 +30,7 @@ public class API {
 
     public static void main(String[] args) {
         String apiKey = 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;
-        String url = // need to figure out what this is
+        String url = "https://nam04.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.kaggle.com%2Fdatasets%2Fuom190346a%2Ffood-ingredients-and-allergens&data=05%7C02%7Camoss1%40mines.edu%7Cd455bb083f5046eaf63f08dd6ea48e99%7C997209e009b346239a4d76afa44a675c%7C0%7C0%7C638788375230259900%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=qeZlohm47VohXbw0QKj1JlpHyHTT60EVVZWzse6slCI%3D&reserved=0"
 
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
