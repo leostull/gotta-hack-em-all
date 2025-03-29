919c2c6754270ba6eb1ca24dbc11f9077dfefff7
pollland <thejusticcegaming@gmail.com>
2025-03-29 01:56:07 -0600

    test with wip camera code

--- MyHelloApp/app/src/main/java/com/example/myhelloapp/MainActivity.java
+++ MyHelloApp/app/src/main/java/com/example/myhelloapp/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.myhelloapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myhelloapp.R; // Replace with your package name
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUESTCODE = 100;
+    Button btnpicture;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnpicture = findViewById(R.id.btncamera_id);
+        imageView = findViewById(R.id.imageview1);
+
+        btnpicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View v) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, REQUESTCODE);
+            }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUESTCODE) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                imageView.setImageBitmap((android.graphics.Bitmap) bundle.get("data"));
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
